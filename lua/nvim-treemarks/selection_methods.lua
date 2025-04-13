27eb9bc697a3d4f4060d7c22b4c2f36a8fb4acee
Aaron <aaronwey@posteo.de>
2025-04-13 12:10:47 +0200

    Feature: Add Telescope UI as a selection method

--- lua/nvim-treemarks/selection_methods.lua
+++ lua/nvim-treemarks/selection_methods.lua
@@ -1,6 +1,6 @@
 local native_ui = require("nvim-treemarks.selection_methods.native_ui")
 
-local telescope_ui = require("nvim-treemarks.selection_methods.native_ui")
+local telescope_ui = require("nvim-treemarks.selection_methods.telescope_ui")
 local module = {
 	NativeUI = native_ui,
 	TelescopeUI = telescope_ui,
--- lua/nvim-treemarks/selection_methods/telescope_ui.lua
+++ lua/nvim-treemarks/selection_methods/telescope_ui.lua
@@ -1,15 +1,43 @@
-local user_config = require("nvim-treemarks.user_config")
-local utility = require("nvim-treemarks.utility")
-
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 local module = {}
 
 function module.setup(args) end
 
 function module.choose(initial_list) end
 function module.choose_async(initial_list, callback)
-	--Todo: Use Telescope
-	local uuids = utility.get_keys(initial_list)
-	vim.ui.input({ prompt = "Enter a uuid to choose a parent:\n" .. table.concat(uuids, "\n") }, callback)
+	local table_as_strs = {}
+	for i, v in pairs(initial_list) do
+		table_as_strs[#table_as_strs + 1] = v.uuid
+	end
+	print(vim.inspect(table_as_strs))
+	pickers
+		.new({}, {
+			prompt_title = "TreeMarks",
+			finder = finders.new_table({
+				results = table_as_strs,
+				-- function(entry)
+				-- 	-- print(vim.inspect(entry))
+				-- 	return { value = entry.uuid, display = entry.uuid }
+				-- end,
+			}),
+			sorter = conf.generic_sorter({}),
+			attach_mappings = function(prompt_bufnr, map)
+				actions.select_default:replace(function()
+					actions.close(prompt_bufnr)
+					local selection = action_state.get_selected_entry()
+					print(vim.inspect(selection))
+					-- vim.api.nvim_put({ selection[1] }, "", false, true)
+					-- As long as we match the original uuid we are fine with this
+					callback(selection[1])
+				end)
+				return true
+			end,
+		})
+		:find()
 end
 
 function module.next_cyclic() end
